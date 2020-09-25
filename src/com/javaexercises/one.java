e3b4df10ae1f4d582771cf1612ba751b974f0e96
ArchieTheRaccoon <71369501+ArchieTheRaccoon@users.noreply.github.com>
2020-09-25 11:42:35 +0200

    Just a new Upload

--- src/com/javaexercises/one.java
+++ src/com/javaexercises/one.java
@@ -2,6 +2,6 @@ package com.javaexercises;
 
 public class one {
     public static void main(String[] Args) {
-        
+        System.out.println("Welcome to SSI Schaefer IT Solutions GmbH");
     }
 }
---
+++ src/com/javaexercises/two.java
@@ -0,0 +1,7 @@
+package com.javaexercises;
+
+public class two {
+    public static void main(String[] Args) {
+
+    }
+}
