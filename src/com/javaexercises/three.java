532f7a630f4eef8f1b95bf02c2d4911945778cae
ArchieTheRaccoon <71369501+ArchieTheRaccoon@users.noreply.github.com>
2020-09-25 13:02:25 +0200

    Third Exercise
    Did it

---
+++ src/com/javaexercises/three.java
@@ -0,0 +1,12 @@
+package com.javaexercises;
+
+public class three {
+    public static void main(String[] Args) {
+        float lengthOfTriangleA = 3;
+        float lengthOfTriangleB = 4;
+        float lengthOfTriangleC = 7;
+        float numberFromHersonsFormula = 2;
+        float areaOfTriangle = (lengthOfTriangleA + lengthOfTriangleB + lengthOfTriangleC) / numberFromHersonsFormula;
+        System.out.println("Result: " + areaOfTriangle);
+    }
+}
