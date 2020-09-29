ded0a4657752729f0dfe9d520180b6232b5e4da9
ArchieTheRaccoon <71369501+ArchieTheRaccoon@users.noreply.github.com>
2020-09-29 15:14:40 +0200

    Update Three.java

--- src/com/javaexercises/Three.java
+++ src/com/javaexercises/Three.java
@@ -2,11 +2,21 @@ package com.javaexercises;
 
 public class Three {
     public static void main(String[] Args) {
-        float lengthOfTriangleA = 3;
-        float lengthOfTriangleB = 4;
-        float lengthOfTriangleC = 7;
-        float numberFromHersonsFormula = 2;
-        float areaOfTriangle = (lengthOfTriangleA + lengthOfTriangleB + lengthOfTriangleC) / numberFromHersonsFormula;
-        System.out.println("Result: " + areaOfTriangle);
+        float lengthOfTriangleA = 10;
+        float lengthOfTriangleB = 10;
+        float lengthOfTriangleC = 10;
+        float inHalfTiling = 2;
+        double halfSize = (lengthOfTriangleA + lengthOfTriangleB + lengthOfTriangleC) / inHalfTiling;
+        double areaOfTriangle = Math.sqrt(
+                halfSize *
+                (halfSize - lengthOfTriangleA) *
+                (halfSize - lengthOfTriangleB) *
+                (halfSize - lengthOfTriangleC));
+
+        if (areaOfTriangle > 0) {
+            System.out.println("Area of this Triangle is " + (float)areaOfTriangle + ".");
+        } else {
+            System.out.println("The Area for this Triangle does not exist.");
+        }
     }
 }
