150a4e828ce0f57b2442c55addf9912890e1932c
ArchieTheRaccoon <71369501+ArchieTheRaccoon@users.noreply.github.com>
2020-10-19 14:16:08 +0200

    Create Nine.java

---
+++ src/com/javaexercises/Nine.java
@@ -0,0 +1,18 @@
+package com.javaexercises;
+
+public class Nine {
+    public static void main(String[] Args) {
+        System.out.println(
+                "\t\t|" +
+                "\n\t\t||" +
+                "\n\t\t|||" +
+                "\n\t\t||||" +
+                "\n\t\t|||||" +
+                "\n|||||||||||||||" +
+                "\n\t\t|||||" +
+                "\n\t\t||||" +
+                "\n\t\t|||" +
+                "\n\t\t||" +
+                "\n\t\t|");
+    }
+}
