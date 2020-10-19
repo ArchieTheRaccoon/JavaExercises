75297ff660597f8383932a236b907da15753d7c1
ArchieTheRaccoon <71369501+ArchieTheRaccoon@users.noreply.github.com>
2020-10-19 13:28:36 +0200

    Create Eight.java

---
+++ src/com/javaexercises/Eight.java
@@ -0,0 +1,11 @@
+package com.javaexercises;
+
+public class Eight {
+    public static void main(String[] Args) {
+        Integer[] numbers = {11, 12, 13, 14, 15};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Number " + (i + 1) + " is " + numbers[i]);
+        }
+        System.out.println("Bitch");
+    }
+}
