c9f28dd0456c8bde5e42295ebd168fc78dc4dc35
ArchieTheRaccoon <71369501+ArchieTheRaccoon@users.noreply.github.com>
2020-10-19 14:09:49 +0200

    Update Eight.java

--- src/com/javaexercises/Eight.java
+++ src/com/javaexercises/Eight.java
@@ -6,6 +6,19 @@ public class Eight {
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("Number " + (i + 1) + " is " + numbers[i]);
         }
-        System.out.println("Bitch");
+        Integer[] calenderDays = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        int countDays = 1;
+
+        for (int i = 0; i < calenderDays.length; i++) {
+            for (int i1 = 0; i1 < calenderDays[i]; i1++) {
+                if (countDays == 6) {
+                    countDays = 0;
+                } else {
+                    countDays++;
+                }
+                System.out.println(daysOfWeek[countDays] + " " + (i1 + 1) + "." + (i + 1) + ".2020");
+            }
+        }
     }
 }
