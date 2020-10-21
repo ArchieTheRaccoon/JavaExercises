958b9fbc4a46abe71a9e8269f8386b7e9ed90ee4
ArchieTheRaccoon <71369501+ArchieTheRaccoon@users.noreply.github.com>
2020-10-21 15:26:14 +0200

    Update Thirteen.java

--- src/com/javaexercises/Thirteen.java
+++ src/com/javaexercises/Thirteen.java
@@ -1,10 +1,7 @@
 
 package com.javaexercises;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Thirteen {
   public static void main(String[] Args) {
@@ -55,7 +52,29 @@ public class Thirteen {
       }
       howManyCopys = 0;
     }
-
     System.out.println("Dublicated names: " + dublicatedNames);
+
+    Map<String, String> phoneBook = new HashMap<String, String>();
+    Thirteen thisClass = new Thirteen();
+
+    for (String classmate : classmates) {
+      phoneBook.put(classmate, thisClass.getRandomPhoneNumber().toString());
+    }
+    System.out.println("Gnar's number: " + phoneBook.get("Gnar"));
+    System.out.println("Elise's number: " + phoneBook.get("Elise"));
+    System.out.println("Zoe's number: " + phoneBook.get("Zoe"));
+    System.out.println("Jinx's number: " + phoneBook.get("Jinx"));
+    System.out.println("Leona's number: " + phoneBook.get("Leona"));
+  }
+  public StringBuilder getRandomPhoneNumber() {
+    StringBuilder phoneNumber = new StringBuilder("+49");
+    for (int i = 0; i < 11; i++) {
+      phoneNumber.append(this.getRandomNumber());
+    }
+    return phoneNumber;
+  }
+  public int getRandomNumber() {
+    Random random = new Random();
+    return random.nextInt(9);
   }
 }
